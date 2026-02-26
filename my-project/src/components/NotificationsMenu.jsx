9561b90a0eb9968ceca8d7858ea6ae0fb4336eae
jellyayce <jayceekentcabansag123@gmail.com>
2026-02-26 22:28:19 +0800

    Added some toggle buttons

--- my-project/src/components/NotificationsMenu.jsx
+++ my-project/src/components/NotificationsMenu.jsx
@@ -5,13 +5,22 @@ export default function NotificationsMenu() {
     <div className='flex flex-col p-6 gap-8 bg-base-200 border border-base-300 rounded-box w-2xl'>
         <h2 className="font-extrabold text-3xl text-black">Notifications</h2>
         <div className='flex flex-col gap-6 text-xl font-medium'>
-          <div>
+          <div className='flex flex-row items-center justify-between'>
             <span>Appointment Reminders</span>
-            
+            <input type="checkbox" defaultChecked className="toggle" />
           </div>
-          <span>Parts Availability Alerts</span>
-          <span>Promotions & Special Deals</span>
-          <span>System Announcements</span>
+          <div className='flex flex-row items-center justify-between'>
+            <span>Parts Availability Alerts</span>
+            <input type="checkbox" defaultChecked className="toggle" />
+          </div>
+          <div className='flex flex-row items-center justify-between'>
+            <span>Promotions & Special Deals</span>
+            <input type="checkbox" defaultChecked className="toggle" />
+          </div>
+          <div className='flex flex-row items-center justify-between'>
+            <span>System Announcements</span>
+            <input type="checkbox" defaultChecked className="toggle" />
+          </div>
         </div>
     </div>
   )
