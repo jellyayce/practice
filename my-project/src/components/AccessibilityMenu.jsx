948a607329d5d27eaba2da40e94728d2562792cb
jellyayce <jayceekentcabansag123@gmail.com>
2026-02-28 23:42:30 +0800

    Added Accessibility

--- my-project/src/components/AccessibilityMenu.jsx
+++ my-project/src/components/AccessibilityMenu.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function AccessibilityMenu() {
   return (
-    <div>
-        <h1>Accessibility</h1>
+    <div className='flex flex-col p-6 gap-8 bg-base-200 border border-base-300 rounded-box w-2xl'>
+        <h2 className="font-extrabold text-3xl text-black">Accessibility</h2>
+        <div className='flex flex-col gap-6 text-xl font-medium'>
+          <div className='flex flex-row items-center justify-between'>
+            <span>High Contrast Mode</span>
+            <input type="checkbox" defaultChecked className="toggle" />
+          </div>
+          <div className='flex flex-row items-center justify-between'>
+            <span>Reduce Motion</span>
+            <input type="checkbox" defaultChecked className="toggle" />
+          </div>
+          <div className='flex flex-row items-center justify-between'>
+            <span>Screen Reader Assistance</span>
+            <input type="checkbox" defaultChecked className="toggle" />
+          </div>
+        </div>
     </div>
   )
 }
