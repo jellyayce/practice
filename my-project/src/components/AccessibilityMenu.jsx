45b13d9807c893fd9a81e289857864c26007dbd0
jellyayce <jayceekentcabansag123@gmail.com>
2026-03-05 21:33:17 +0800

    Centered settings components

--- my-project/src/components/AccessibilityMenu.jsx
+++ my-project/src/components/AccessibilityMenu.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function AccessibilityMenu() {
   return (
-    <div className='flex flex-col p-6 gap-8 bg-base-200 border border-base-300 rounded-box w-2xl'>
+    <div className="flex justify-center flex-1">
+      <div className="flex flex-col p-6 gap-8 bg-base-200 border border-base-300 rounded-box w-2xl">
         <h2 className="font-extrabold text-3xl text-black">Accessibility</h2>
-        <div className='flex flex-col gap-6 text-xl font-medium'>
-          <div className='flex flex-row items-center justify-between'>
+        <div className="flex flex-col gap-6 text-xl font-medium">
+          <div className="flex flex-row items-center justify-between">
             <span>High Contrast Mode</span>
             <input type="checkbox" defaultChecked className="toggle" />
           </div>
-          <div className='flex flex-row items-center justify-between'>
+          <div className="flex flex-row items-center justify-between">
             <span>Reduce Motion</span>
             <input type="checkbox" defaultChecked className="toggle" />
           </div>
-          <div className='flex flex-row items-center justify-between'>
+          <div className="flex flex-row items-center justify-between">
             <span>Screen Reader Assistance</span>
             <input type="checkbox" defaultChecked className="toggle" />
           </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
--- my-project/src/components/NotificationsMenu.jsx
+++ my-project/src/components/NotificationsMenu.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function NotificationsMenu() {
   return (
-    <div className='flex flex-col p-6 gap-8 bg-base-200 border border-base-300 rounded-box w-2xl'>
+    <div className="flex justify-center flex-1">
+      <div className="flex flex-col p-6 gap-8 bg-base-200 border border-base-300 rounded-box w-2xl">
         <h2 className="font-extrabold text-3xl text-black">Notifications</h2>
-        <div className='flex flex-col gap-6 text-xl font-medium'>
-          <div className='flex flex-row items-center justify-between'>
+        <div className="flex flex-col gap-6 text-xl font-medium">
+          <div className="flex flex-row items-center justify-between">
             <span>Appointment Reminders</span>
             <input type="checkbox" defaultChecked className="toggle" />
           </div>
-          <div className='flex flex-row items-center justify-between'>
+          <div className="flex flex-row items-center justify-between">
             <span>Parts Availability Alerts</span>
             <input type="checkbox" defaultChecked className="toggle" />
           </div>
-          <div className='flex flex-row items-center justify-between'>
+          <div className="flex flex-row items-center justify-between">
             <span>Promotions & Special Deals</span>
             <input type="checkbox" defaultChecked className="toggle" />
           </div>
-          <div className='flex flex-row items-center justify-between'>
+          <div className="flex flex-row items-center justify-between">
             <span>System Announcements</span>
             <input type="checkbox" defaultChecked className="toggle" />
           </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
